6309a0b897a9bbccd6a0673d5afc3513ac88e294
Zoresss <140959998+Zoresss@users.noreply.github.com>
2026-05-21 22:19:38 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const fileInput = document.getElementById('fileInput');
 const preview1 = document.getElementById('preview1');
 const preview2 = document.getElementById('preview2');
+const text1 = document.getElementById('text1');
+const text2 = document.getElementById('text2');
 let clickCount = 0;
 
 fileInput.addEventListener('change', function() {
@@ -11,12 +13,16 @@ fileInput.addEventListener('change', function() {
             if (clickCount % 2 === 0) {
                 preview1.src = e.target.result;
                 preview1.style.display = 'block';
+                text1.style.display = 'none'; // Скрываем текст "нажмите для загрузки"
             } else {
                 preview2.src = e.target.result;
                 preview2.style.display = 'block';
+                text2.style.display = 'none'; // Скрываем текст "нажмите для загрузки"
             }
             clickCount++;
         }
         reader.readAsDataURL(file);
     }
+    // Сброс значения, чтобы можно было загрузить одно и то же фото дважды
+    this.value = ''; 
 });
